51dd7ff714bbdc8db7b97b1a0730ce07749f1238
Dilip Kola <kdilipkola@gmail.com>
2024-03-14 21:31:09 +0530

    fix: version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,13 @@ plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("jacoco")
-    id("pl.allegro.tech.build.axion-release") version "1.15.1"
+    id("pl.allegro.tech.build.axion-release") version "1.17.0"
     id("org.sonarqube") version "4.4.1.3373"
+}
+scmVersion {
+    repository {
+        pushTagsOnly.set(false)
+    }
 }
 
 project.version = scmVersion.version
@@ -13,6 +18,7 @@ group = "com.rudderstack"
 repositories {
     mavenCentral()
 }
+
 val kafkaVersion = "3.7.0"
 val slf4jApiVersion = "2.0.12"
 val junitVersion = "5.10.2"
@@ -48,6 +54,7 @@ sonar {
     property("sonar.projectKey", "rudderlabs_rudder-kafka-sink-connector")
     property("sonar.organization", "rudderlabs")
     property("sonar.host.url", "https://sonarcloud.io")
+    property("sonar.gradle.skipCompile", "true")
   }
 }
 
